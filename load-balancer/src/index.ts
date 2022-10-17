3bd203268190540a55421778367f725e1be64c14
Juubes <grumpt1@gmail.com>
2022-10-17 20:53:07 +0300

    Partitioning image

--- load-balancer/src/index.ts
+++ load-balancer/src/index.ts
@@ -42,98 +42,9 @@ app.get("/", (req, res) => {
   if (endY <= startY || endX <= startX || imgWidth * imgHeight < 1) {
     return res.end(400);
   }
-  const maxIterCount = 100 + Math.pow(imgWidth / (endX - startX), 0.4);
 
   console.log("Calculating for...");
   console.log({ startX, startY, endX, endY, imgWidth, imgHeight });
 
-  // Get pixels in rectangle
-  const iterations = new Array<Array<number>>(imgHeight);
-
-  const incrX = (endX - startX) / imgWidth;
-  const incrY = (endY - startY) / imgHeight;
-
-  let indexX = 0;
-
-  const timeStart = Date.now();
-  for (let x = startX; x < endX; x += incrX) {
-    let indexY = 0;
-
-    iterations[indexX] = new Array(imgWidth);
-    for (let y = startY; y < endY; y += incrY) {
-      let normalizedX = (x / imgWidth) * 4;
-      let normalizedY = (y / imgHeight) * 3;
-
-      // console.log("Calculate pixel " + indexX + ":" + indexY);
-
-      iterations[indexX][indexY] = Math.min(
-        getIterations(normalizedX, normalizedY, maxIterCount),
-        255
-      );
-      indexY++;
-    }
-    indexX++;
-  }
-  const timeEnd = Date.now();
-
-  console.log(timeEnd - timeStart + "ms for fractal calculation");
-
-  // let MAX_ITER_COUNT_IN_DATA = 255;
-  const data: number[] = [];
-  for (let i = 0; i < imgHeight; i++) {
-    for (let j = 0; j < imgWidth; j++) {
-      data.push(iterations[j][i]);
-
-      // if (iterations[j][i] > MAX_ITER_COUNT_IN_DATA)
-      //   MAX_ITER_COUNT_IN_DATA = iterations[j][i];
-    }
-  }
-
-  // for (let i = 0; i < data.length; i++) {
-  //   const element = data[i];
-  //   data[i] = (255 * element) / MAX_ITER_COUNT_IN_DATA;
-  // }
-
-  console.log("Iterations per pixel: " + maxIterCount);
-  // const clamped = Uint8ClampedArray.from(data);
-
-  const clamped = Uint32Array.from(data);
-
-  res.send(Buffer.from(clamped.buffer));
-});
-
-// TODO: colors
-const getIterations = (
-  argX: number,
-  argY: number,
-  maxIterCount: number
-): number => {
-  // Offsets so the image shows up at the center
-  const offsetX = -3;
-  const offsetY = -1.5;
-
-  // The pixel to be calculated
-  const pixel = { x: argX + offsetX, y: argY + offsetY };
-
-  let result = { x: 0, y: 0 };
-  let iter_num = 0;
-  let p, distance;
-  do {
-    p = {
-      x: result.x * result.x - result.y * result.y,
-      y: 2 * result.x * result.y,
-    };
-    result = {
-      x: p.x + pixel.x,
-      y: p.y + pixel.y,
-    };
-    distance = Math.sqrt(result.x * result.x + result.y * result.y);
-    iter_num++;
-  } while (distance <= 2 && iter_num < maxIterCount);
-
-  return iter_num;
-};
-
-app.listen(5000, "localhost", () => {
-  console.log("Server started on port 5000");
+  // Divide image into smaller pieces of 10000 pixels
 });
--- rust-executor/src/main.rs
+++ rust-executor/src/main.rs
@@ -1,22 +1,14 @@
 pub mod mandelbrot;
+pub mod process_request;
+pub mod zoom_params;
 
-use serde::{Deserialize, Serialize};
-use std::{time::Instant, vec};
+use zoom_params::ZoomParams;
 
 use warp::{
     hyper::{Method, Response},
     Filter,
 };
 
-#[derive(Deserialize, Serialize, Debug)]
-struct ZoomParams {
-    start_x: f64,
-    start_y: f64,
-    end_x: f64,
-    end_y: f64,
-    img_width: u16,
-    img_height: u16,
-}
 impl core::fmt::Display for ZoomParams {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(
@@ -38,7 +30,7 @@ async fn start_server() {
     let index = warp::get()
         .and(warp::path::end()) // index
         .and(warp::query::<ZoomParams>())
-        .then(process_request);
+        .map(process_request::process_request);
 
     let catch_all = warp::any().map(|| Response::builder().status(404).body("404"));
 
@@ -50,64 +42,7 @@ async fn start_server() {
     warp::serve(routes).run(([127, 0, 0, 1], 5000)).await;
 }
 
-async fn process_request(params: ZoomParams) -> Vec<u8> {
-    let ZoomParams {
-        start_x,
-        start_y,
-        end_x,
-        end_y,
-        img_width,
-        img_height,
-    } = params;
-
-    let img_width = img_width as f64;
-    let img_height = img_height as f64;
-
-    if end_y <= start_y || end_x <= start_x || img_width * img_height < 1.0 {
-        // TODO: handle properly with code 400
-        panic!("Invalid input")
-    }
-
-    // let max_iter_count = ((100.0 + (img_width / (end_x - start_x)).powf(0.5)).floor()) as u32;
-    let scale = (end_x - start_x) / img_width;
-    let max_iter_count = (50.0 + f64::log10(4.0 / f64::abs(scale)).powf(5.0)) as u32;
-
-    println!("Max iterations: {}", max_iter_count);
-
-    println!("Calculating area for...");
-    println!(
-        "{{ startX: {}, startY: {}, endX: {}, endY: {}, imgWidth: {}: imgHeight: {}}}",
-        start_x, start_y, end_x, end_y, img_width, img_height
-    );
-
-    let mut iterations = vec![255; (img_width as usize) * (img_height as usize)];
-
-    let time_start = Instant::now();
-    let range_x = (end_x - start_x).abs();
-    let range_y = (end_y - start_y).abs();
-
-    println!("range_x: {}", range_x);
-
-    for pixel_x in 0..img_width as u16 {
-        for pixel_y in 0..img_height as u16 {
-            let x = (start_x + (pixel_x as f64) / img_width * range_x) / img_width;
-            let y = (start_y + (pixel_y as f64) / img_height * range_y) / img_height;
-
-            iterations[((pixel_y as f64) * img_width) as usize + (pixel_x as usize)] =
-                mandelbrot::get_iterations(x, y, max_iter_count);
-        }
-    }
-
-    let time_end = time_start.elapsed();
-    println!("Calculated frame in {}ms", time_end.as_millis());
-
-    let vec: Vec<u8> = iterations.iter().flat_map(|e| e.to_le_bytes()).collect();
-
-    return vec;
-}
-
 #[tokio::main]
 async fn main() {
     start_server().await
 }
-// http://localhost:5000/?start_x=0&start_y=0&end_x=1920&end_y=1080&img_width=1920&img_height=1080
